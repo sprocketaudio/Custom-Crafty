dbe5581e5a6b477a75b297c344f57da445524624
Zedifus <zedifus@zediverse.gg>
2024-03-10 22:52:23 +0000

    Appease Black

--- app/classes/shared/migration.py
+++ app/classes/shared/migration.py
@@ -372,8 +372,8 @@ class MigrationManager(object):
         Create migrator
         """
         migrator = Migrator(self.database)
-        # Removing the up_one to prevent running all 
-        # migrations each time we got a new one. 
+        # Removing the up_one to prevent running all
+        # migrations each time we got a new one.
         # It's handled by migration.up() function.
         # for name in self.done:
         #    self.up_one(name, migrator, True)
