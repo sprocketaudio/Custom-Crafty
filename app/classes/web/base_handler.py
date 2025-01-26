7e5eb850de664f252e447659f1de348264ddf674
Andrew <mcdeweykp@gmail.com>
2025-01-26 10:34:39 -0500

    Add check for both token_id and session_id

--- app/classes/web/base_handler.py
+++ app/classes/web/base_handler.py
@@ -201,6 +201,19 @@ class BaseHandler(tornado.web.RequestHandler):
             api_key, token_data, user = self.controller.authentication.check_err(
                 self._auth_get_api_token()
             )
+            if token_data.get("token_id") and token_data.get(
+                "session_id"
+            ):  # Check to see if token defines session_id and token_id
+                return self.finish_json(
+                    403,
+                    {
+                        "status": "error",
+                        "error": "ACCESS_DENIED",
+                        "error_data": self.helper.translation.translate(
+                            "error", "duplicateId", user["lang"]
+                        ),
+                    },
+                )
             if (
                 (user["superuser"] and not token_data.get("mfa"))
                 and not self.is_totp_method()
