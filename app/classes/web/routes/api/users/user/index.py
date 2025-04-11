bdb1729f32cc627dc263f14cc8853a8d75cb96fd
Andrew <mcdeweykp@gmail.com>
2025-04-11 19:13:46 -0400

    Remove totp data on user delete

--- app/classes/web/routes/api/users/user/index.py
+++ app/classes/web/routes/api/users/user/index.py
@@ -75,12 +75,12 @@ class ApiUsersUserIndexHandler(BaseApiHandler):
             user,
             _,
         ) = auth_data
-
+        can_delete = False
         if (user_id in ["@me", user["user_id"]]) and self.helper.get_setting(
             "allow_self_delete", False
         ):
             user_id = user["user_id"]
-            self.controller.users.remove_user(user_id)
+            can_delete = True
         elif EnumPermissionsCrafty.USER_CONFIG not in exec_user_crafty_permissions:
             return self.finish_json(
                 400,
@@ -91,7 +91,21 @@ class ApiUsersUserIndexHandler(BaseApiHandler):
             )
         else:
             # has User_Config permission
-            self.controller.users.remove_user(user_id)
+            can_delete = True
+
+        if not can_delete:
+            return self.finish_json(
+                400,
+                {
+                    "status": "error",
+                    "error": "NOT_AUTHORIZED",
+                },
+            )
+        user_model = self.controller.users.get_user_object(user_id)
+        for totp in list(user_model.totp_user):
+            self.controller.totp.delete_user_totp(totp)
+        self.controller.totp.remove_all_recovery_codes(user_model.user_id)
+        self.controller.users.remove_user(user_id)
 
         self.controller.management.add_to_audit_log(
             user["user_id"],
